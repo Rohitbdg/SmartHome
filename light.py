a95a355628dd29f8afd829be383f9af83bacf017
Rohitbdg <rht7768@gmail.com>
2017-03-17 16:52:39 +0530

    Update light.py

--- light.py
+++ light.py
@@ -7,14 +7,14 @@ def xchangemode():
     x=0
 
 def ledon():
-#	pwm.write(1)
+	pwm.write(1)
     xchangemode()
     print "led turned on"
 
 def ledoff():
     xchangemode()
     print "led turned off"
-#	pwm.write(0)
+	pwm.write(0)
 
 
 
@@ -23,13 +23,11 @@ def ldr_getvalues():
     global x
     x=1
     while x==1:
+	val=light.value()
+	print val
+	if val<100:
+		ledon()
+	else:
+		ledoff()
 	time.sleep(0.3)
-	print "ldr values"
-#	while x==1:
-#		val=light.value()
-#		time.sleep(0.3)
-#		print "ldr values"
-#		if val<100:
-#			ledon()
-#		else :
-#			ledoff()
+	
